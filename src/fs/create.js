0b1af1bbe30d24611d33e1c32ed71999861097b6
Ilya Okunev <zhoramc1@gmail.com>
2024-09-30 21:07:49 +0300

    init: repo with solution of 1 task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs'
+
+const FILE_CONTENT = 'I am fresh and young';
+const FILE_PATH = './files/fresh.txt';
+
+
 const create = async () => {
-    // Write your code here 
+   fs.access(FILE_PATH, fs.constants.F_OK, (err) => {
+        if (err) {
+            fs.writeFile(FILE_PATH, FILE_CONTENT, () => {});
+        } else {
+            throw new Error('FS operation failed')
+        }
+    })
 };
 
 await create();
